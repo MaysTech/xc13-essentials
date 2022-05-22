6e8f01592fa5dfb4c3de9358c2fd429cc38b26a8
Mays Tech <maystechus@gmail.com>
2022-05-21 18:35:16 -0700

    Initial Commit

---
+++ MaysSourceControl/MaysSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaysSourceControlApp.swift
+//  MaysSourceControl
+//
+//  Created by May Mascenik on 5/21/22.
+//
+
+import SwiftUI
+
+@main
+struct MaysSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
